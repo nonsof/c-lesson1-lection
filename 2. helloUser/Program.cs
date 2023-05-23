7d94661ae3ec581538af4f425241c87c768afc65
non sof <sofinalove2012@gmail.com>
2023-05-23 22:40:04 +0300

    userlastname

--- 2. helloUser/Program.cs
+++ 2. helloUser/Program.cs
@@ -2,3 +2,8 @@
 string username = Console.ReadLine();
 Console.WriteLine ("Привет, ");
 Console.WriteLine (username);
+Console.Write("Введите Вашу фамилию!");
+string userlastname = Console.ReadLine();
+Console.Write ("Привет, ");
+Console.Write (username);
+Console.Write (userlastname);
